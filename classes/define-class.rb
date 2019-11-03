3e729d484e5b71ef79632f27721504f67fa6db4f
cristiano <cs.almeida@protonmail.ch>
2019-11-03 22:19:56 +0000

    Define class file. 💎

---
+++ classes/define-class.rb
@@ -0,0 +1,14 @@
+#!usr/bin.env ruby
+
+# Use the class method to know what type an object is.
+word = 'Ruby'
+puts word.class # String
+
+
+# A class is defined by the class keyword.
+# It typically lives in its own file.
+class ShoppingCart
+  #...code here
+end
+
+puts ShoppingCart.class
