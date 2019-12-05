8eb7c068918a9ca96645e4d335ca43f77a9f8d25
Cristiano Almeida <cs.almeida@protonmail.ch>
2019-12-05 15:45:17 +0000

    Method access control. 💎

--- ruby_project/classes/person.rb
+++ ruby_project/classes/person.rb
@@ -1,5 +1,20 @@
 class Person
+  attr_accessor :first_name, :last_name
+
   def greet
     'Hello!'
   end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end  
+
+  def initial_and_last_name
+    "#{get_initial(first_name)} #{last_name}"
+  end  
+
+  private
+    def get_initial(name)
+      name.chars.first + '.'
+    end
 end
--- ruby_project/init.rb
+++ ruby_project/init.rb
@@ -8,6 +8,11 @@ require_relative 'classes/animal'
 person = Person.new
 puts person.greet
 
+person.first_name = "Han"
+person.last_name = "Solo"
+# Method makes use of private method in the class.
+puts person.initial_and_last_name
+
 # Creating an animal class making use of its @noise attribute.
 pig = Animal.new
 pig.noise = "Oink!"
