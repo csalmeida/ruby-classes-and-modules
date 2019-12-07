db4bc350f9bdbb7e115c50405e6c9cc0a0004f40
cristiano <cs.almeida@protonmail.ch>
2019-12-07 23:06:45 +0000

    Display result is not bound to two die. ⚁ This allows any number of dice to be passed to DiceSet.

--- challenges/dice/solution/classes/dice_set.rb
+++ challenges/dice/solution/classes/dice_set.rb
@@ -2,8 +2,6 @@ class DiceSet
   def initialize(options={})
     # Received two dice in an array.
     @dice = options[:dice]
-    # Sets initial dice values.
-    set_value
   end
 
   # Rolls each die present in the dice array.
@@ -11,17 +9,10 @@ class DiceSet
     @dice.map do |die|
       die.roll
     end
-    set_value
   end
 
   # Used to consult current results without doing another roll.
   def display
-    @value
+    @dice.map {|die| "[ #{die.value} ]" }.join(' - ')
   end
-  
-  # Set's value of set. Not available outside instance.
-  private
-    def set_value()
-      @value = "[#{@dice.first.value}, #{@dice.last.value}]"
-    end
 end
