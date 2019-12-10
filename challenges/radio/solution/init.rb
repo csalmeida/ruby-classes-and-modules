6eae9481ba2f04b7b59e5813046452f0e5801d56
Cristiano Almeida <cs.almeida@protonmail.ch>
2019-12-10 12:45:51 +0000

    FM and AM radio script. 📻 Making use of radio class.

--- challenges/radio/solution/init.rb
+++ challenges/radio/solution/init.rb
@@ -2,14 +2,30 @@
 
 require_relative 'classes/radio'
 
-fm = Radio.new
+# FM Radio
+puts "Creating an FM radio..."
+fm = Radio.fm
+puts fm.status
 
-puts fm.volume
-fm.volume = 11
-puts fm.volume
-fm.volume = 3
-puts fm.volume
+puts "Lowering volume..."
+fm.volume = 1
+puts fm.status
 
-puts Radio.bands_range
-puts fm.band
-puts fm.freq
+puts "Changing station..."
+fm.freq = 92.2
+puts fm.status
+
+puts "---------------------------------"
+
+# AM Radio
+puts "Creating an AM radio..."
+am = Radio.am
+puts am.status
+
+puts "Increasing volume..."
+am.volume = 10
+puts am.status
+
+puts "Changing station..."
+am.freq = 647.9
+puts am.status
