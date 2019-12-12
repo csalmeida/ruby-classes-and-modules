37eae30a3cbfe2f2518121c2cd987a1755a598da
Cristiano Almeida <cs.almeida@protonmail.ch>
2019-12-12 14:57:37 +0000

    Adds comments to override/extend file. 💎

--- inheritance/override_extend.rb
+++ inheritance/override_extend.rb
@@ -1,5 +1,6 @@
 #!usr/bin/env ruby
 
+# Superclass definition. 
 class Sofa
   @@can_open = false
   attr_accessor :width, :length
@@ -9,15 +10,22 @@ class Sofa
   end
 end
 
+# Subclass inherits from Sofa.
 class SofaBed < Sofa
+  # Overrites its class attribute.
   @@can_open = true
+
+  # Extends itself by adding two new instance attributes.
   attr_accessor :length_opened
   attr_reader  :is_open
 
+  # Overrides area to calculate when bed is open.
   def area
     @is_open ? width * @length_opened : width * length
   end
   
+  # Extends class further by other two additional methods.
+  # These methods to not exist in the Sofa parent class.
   def open
     @is_open = true
   end
