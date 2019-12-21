b3ba8a5895b6919414e441a6df975873e65a2aa7
cristiano <cs.almeida@protonmail.ch>
2019-12-21 15:57:14 +0000

    SecureRadio#audio_stream is protected. 💎

--- challenges/secure-radio/solution/classes/secure_radio.rb
+++ challenges/secure-radio/solution/classes/secure_radio.rb
@@ -2,7 +2,9 @@ require_relative 'shift_cipher'
 require_relative 'radio'
 
 class SecureRadio < Radio
-  def audio_stream
-    ShiftCipher.encode(super)
-  end
+  protected
+
+    def audio_stream
+      ShiftCipher.encode(super)
+    end
 end
