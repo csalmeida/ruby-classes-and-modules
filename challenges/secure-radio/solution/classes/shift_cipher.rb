ecb8442a4b288fce76b72ecba43e2c7079d99082
cristiano <cs.almeida@protonmail.ch>
2019-12-21 15:50:33 +0000

    ShiftCipher deals with any character. 💎 It will check if a character can be shifted before trying to do so.

--- challenges/secure-radio/solution/classes/shift_cipher.rb
+++ challenges/secure-radio/solution/classes/shift_cipher.rb
@@ -2,39 +2,33 @@ class ShiftCipher
   @@alphabet = [*'a'..'z']
 
   # The encode method takes a plain string and shifts it by a certain amount of characters.
-  def self.encode(plain_string, shift=3)
+  def self.encode(string, shift=3)
     # Takes each word and splits it into charactes to be shifted.
-    words = plain_string.split
-    encoded_words = self.shift(words, shift)
-    # Turns encoded array back into a string.
-    encoded_words.join
+    encoded_words = self.shift(string, shift)
   end
 
   # Reverts an encoded string. Requires previous used shift.
-  def self.decode(cipher_string, shift=3)
-    words = cipher_string.split
-    decoded_words = self.shift(words, shift, true)
-    decoded_words.join
+  def self.decode(string, shift=3)
+    decoded_words = self.shift(string, shift, true)
   end
 
   private
 
     # Shifts characters. Used in encoding and decoding.
-    def self.shift(words, shift, revert=false)
-      words.map.with_index do |word, index|
-        # Each character is shifted using @@alphabet.
-        new_word = word.split("").map do |char|
+    def self.shift(string, shift, revert=false)
+      # Each character is shifted using @@alphabet.
+      string.split("").map do |char|
+        if @@alphabet.include?(char.downcase)
           # Position of letter in the alphabet with shift applied.
           alpha_index = !revert ? @@alphabet.find_index(char.downcase) + shift : @@alphabet.find_index(char.downcase) - shift
           # If character shift falls out of alphabet, uses reverse shift.
           shifted_char = alpha_index < @@alphabet.length ? @@alphabet[alpha_index] : @@alphabet[alpha_index % @@alphabet.length]
           # Returns the shifted character with correct casing.
           self.is_upcase?(char) ? shifted_char.upcase : shifted_char
+        else
+          char
         end
-        # Adds space to each word but the last.
-        space = (index + 1) != words.count ? " " : ""
-        new_word.join + space
-      end
+      end.join
     end
 
     # Checks if a string is upcase.
