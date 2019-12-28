fb16ac81d7a134b4be4eb91794ca20353ddc4f26
cristiano <cs.almeida@protonmail.ch>
2019-12-28 18:43:40 +0000

    Moved name related behavior to its own module. 💎

---
+++ ruby_project/modules/nameable.rb
@@ -0,0 +1,16 @@
+module Nameable
+  attr_accessor :first_name, :last_name
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end  
+
+  def initial_and_last_name
+    "#{get_initial(first_name)} #{last_name}"
+  end  
+
+  private
+    def get_initial(name)
+      name.chars.first + '.'
+    end
+end
