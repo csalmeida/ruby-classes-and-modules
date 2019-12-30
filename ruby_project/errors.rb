eb0818faf801aaa50ec1d054a364b5515ff9a95e
cristiano <cs.almeida@protonmail.ch>
2019-12-30 13:27:18 +0000

    Added custom exceptions to errors. 💎

--- ruby_project/errors.rb
+++ ruby_project/errors.rb
@@ -20,8 +20,18 @@ begin
 #   puts "Requires two integer arguments"
 end
 
-def even_numbers(array)
+class NoEvenNumbers < StandardError
   
+  attr_accessor :array
+
+  def initialize(array)
+    super("No even numbers")
+    @array = array
+  end
+end
+
+def even_numbers(array)
+
   unless array.is_a?(Array)
     raise ArgumentError
   end
@@ -30,9 +40,22 @@ def even_numbers(array)
     raise StandardError.new("Too few elements")
   end
 
-  array.find_all {|item| item.to_i % 2 == 0}
+  even_array = array.find_all {|item| item.to_i % 2 == 0}
+
+  if even_array.length == 0
+    raise NoEvenNumbers.new(array)
+  end
+
+  return even_array
 end
 
 puts even_numbers([*1..5]).join(',')
 # puts even_numbers(1..20)
-puts even_numbers([])
+# puts even_numbers([])
+
+begin
+  puts even_numbers([1,3,5,7])
+rescue => e
+  puts "#{e.class}: #{e.message}"
+  puts "The array provided was: [#{e.array.join(",")}]"
+end
